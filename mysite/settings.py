e09a090ee7234513ee859ccb6e485b20ce2ce5d2
kaR07L6 <u.rinu.mao0624@icloud.com>
2024-11-06 10:59:54 +0900

    その8(Django Debug Toolbar)

--- mysite/settings.py
+++ mysite/settings.py
@@ -56,10 +56,11 @@ ROOT_URLCONF = "mysite.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        "DIRS": [BASE_DIR / "templates"],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
+                "django.template.context_processors.debug",
                 "django.template.context_processors.request",
                 "django.contrib.auth.context_processors.auth",
                 "django.contrib.messages.context_processors.messages",
@@ -67,7 +68,6 @@ TEMPLATES = [
         },
     },
 ]
-
 WSGI_APPLICATION = "mysite.wsgi.application"
 
 
