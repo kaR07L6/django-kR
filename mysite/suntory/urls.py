72259ea5f911cec5f31c41c995cb0cafb6f756e2
kaR07L6 <u.rinu.mao0624@icloud.com>
2025-02-02 19:40:58 +0900

    サントリー

---
+++ mysite/suntory/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path("index/", views.index, name="index"),  # 一覧、トップページ
+    path("", views.new, name="new"),  # 新規投稿
+]
---
+++ mysite/suntory/views.py
@@ -0,0 +1,19 @@
+from django.shortcuts import render, redirect
+from .forms import MainForm
+from .models import Main
+
+
+def index(request):
+    mains = Main.objects.all().order_by("content")
+    return render(request, "suntory/index.html", {"mains": mains})
+
+
+def new(request):
+    if request.method == "POST":
+        form = MainForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect("index")
+    else:
+        form = MainForm()
+    return render(request, "suntory/new.html", {"form": form})
